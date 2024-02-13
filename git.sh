94204e43cce7c9c14aa06c519099facd55e18da4
hary868 <mhk867@gmail.com>
2024-02-13 14:02:40 +0530

    third push of everything

--- git.sh
+++ git.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-commit='first push of everything'
-url='https://github.com/hary867/Learn-DevOps.git'
+git remote remove origin
+commit='third push of everything'
+#reponame=''
+url="https://github.com/hary8678/Notes-$1.git"
 git init
 git add .
 git commit -m "$commit"
 git branch -M main
 git remote add origin $url
 git push -u origin main
+
